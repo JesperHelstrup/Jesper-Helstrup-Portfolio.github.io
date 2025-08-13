6a23b32b0c03d376d49dbfda99dbb92506b2cf65
Jesper Helstrup <jesper_helstrup@hotmail.com>
2025-08-13 23:08:21 +0200

    vault backup: 2025-08-13 23:08:21

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,8 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/JesperHelstrup",
+      "Linkedin": "https://www.linkedin.com/in/jesper-h-a4702390/",
     },
   }),
 }
